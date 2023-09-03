c7991be05fe9228ae1db1e3668991870ed540765
Network4Pro <youssefelkhanchoufi@gmail.com>
2023-09-03 23:22:53 +0100

    modfier

---
+++ PHP_Form.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="" >
+        <input type="text" > 
+        <input type="" >
+    </form>
+</body>
+</html>
+
+
+
+
+<?php 
+
+/* 
+    Form:
+    -----
+
+    * Un formulaire HTML est utilisé pour collecter les entrées des utilisateurs.
+    * Les entrées de l'utilisateur sont le plus souvent envoyées à un serveur pour traitement.
+
+    - L'élément Html <form> :
+    -------------------------  
+        * Utilisé pour créer un formulaire HTML pour la saisie de l'utilisateur .
+        * Un conteneur pour différents types d'éléments de saisie.
+    
+    - L'élément <input> : élément peut être affiché de plusieurs manières, selon l' type attribut.
+        type : 
+        ------
+        * Text : 
+        * 
+
+
+
+
+
+
+*/
+
+?>
